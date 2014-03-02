be61f40732ef6617c8d0884f0a997f85be31b634
must-lleibl <mustafashahdzahan@gmail.com>
2014-03-02 15:41:54 +0400

    preparing to srart...
    Lets got to start))

---
+++ MArea.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+__PocketMine Plugin__
+name=MArea
+description=Just the best world protection you ever seen
+version=private plugin editing p1
+author=must and xktiverz
+class=MArea
+apiversion=12,13,14
+*/
+
+/*
+//in line *version* will have to write in what percent will progres goes\\
+//next version like=private plugin editing p2\\
+*/
+
+/*
+here you must write ^whats news goes by coding^
+
+*/
+lets get started then...
