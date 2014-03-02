851ba3240bd81bc081c5008c2e231e3e9bf4f7e2
must-lleibl <mustafashahdzahan@gmail.com>
2014-03-02 17:32:26 +0400

    who starting next? I Dont know(((
    little changes

--- MArea.php
+++ MArea.php
@@ -4,7 +4,7 @@
 __PocketMine Plugin__
 name=MArea
 description=Just the best world protection you ever seen
-version=private plugin editing p3
+version=private plugin editing p1
 author=must and xktiverz
 class=MArea
 apiversion=12,13
@@ -12,15 +12,16 @@ apiversion=12,13
 
 /*
 //in line *version* will have to write in what percent will progres goes\\
-//next version like=private plugin editing p2\\
+//next version like=private plugin editing p2(if you editing the code just change the version) \\
 */
 
 /*
-here you must write ^whats news goes by coding^
-
+LETS GET STARTED THEN
 */
 
+
 class MArea implements Plugin{
+
 	public function __construct(ServerAPI $api, $s = 0) {
 	}
 	public function init() {
