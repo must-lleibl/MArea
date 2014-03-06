d60b088b8e78e9114001426c2e2e5afe9570c8a6
PEMapModder <pemapmodder1970@gmail.com>
2014-03-06 18:02:45 +0800

    p57

--- MArea.php
+++ MArea.php
@@ -4,17 +4,18 @@
 __PocketMine Plugin__
 name=MArea
 description=Just the best world protection you ever seen
-version=private plugin editing p15
-author=must and xktiverz
+version=private plugin editing p57
+author=must and PEMapModder
 class=MAreaPg
 apiversion=12,13
 */
 
 /*
-//in line *version* will have to write in what percent will progress goes
 Here is the progress record:
-Created MArea class and permission checker TODO split the in-area/in-space checker into an independent function
-To decide: area (2D) or space (3D)
+p15:	Created MArea class and permission checker TODO split the in-area/in-space checker into an independent function
+		To decide: area (2D) or space (3D)
+p57:	Guranteed customization!
+		Optimized I/O efficiency
 */
 
 
@@ -23,42 +24,132 @@ class MAreaPg implements Plugin {
 	public static &request() {
 		return self::$instance;
 	}
+	public $a, $c;
 	public $areas = array();
+	public $config = false;
+	public $selections = array();
 	public function __construct(ServerAPI $api, $s = 0) {
+		$this->a=$api;
+		$this->c=$api->console;
 	}
 	public function init() {
+		$noExt=$this->a->plugin->configPath($this)."settings.";
+		$ext="yml";
+		if(file_exists($noExt."txt"))
+			$ext="txt";
+		$this->config = new Config($noExt.$ext, CONFIG_YAML, array(
+				"MArea main command" => "marea", // at ConsoleAPI.php trim(strtolower($cmd)) is used
+				"alias" => array(
+						"ma"
+				),
+		));
+		$this->c->register($this->config->get("MArea main command"), "<help|subcmd> MArea main command", array($this, "cmds"));
+		foreach($this->config->get("alias") as $alias)
+			$this->c->alias($alias, $this->config->get("MArea main command"));
+		$this->initMAreas();
+	}
+	public function initMAreas(){
+		$path=$this->a->plugin->configPath($this)."MAreas/";
+		@mkdir($path);
+		$dir = dir($path);
+		while(($f = $dir->read()) !== false){
+			if(substr($f, 0, 6) == "MArea-" and substr($f, -4) == ".yml")
+				$this->areas[] = new MArea($path.$file);
+		}
 	}
 	public function __destruct() {
 	}
 }
 class MArea {
+	const FLAG_NONE = 0x00;
+	const FLAG_ENTER_FREE = 0x01;
+	const FLAG_PVP_FREE = 0x02;
+	const FLAG_FURNACE_ALLOW = 0x14;
+	const FLAG_CHEST_ALLOW = 0x12;
+	const FLAG_CONTAINER_ALLOW = FLAG_FURNACE_ALLOW | FLAG_CHEST_ALLOW;
+	const FLAG_ALL = 0xffff;
+	private $external = false;
 	public $owner;
 	public $owners = array();
-	public $flag = 0; // planned to use like flags like E_ERROR and SORT_FLAG_CASE
+	public $flag = self::FLAG_NONE; // planned to use like flags like E_ERROR and SORT_FLAG_CASE
 	public $start, $end;
-	public function __construct(Position $start, Position $end, Player $owner) {
-		$this->start = $start;
-		$this->end = $end;
-		$this->owner = $owner;
-		ServerAPI::request()->addHandler("player.block.touch", array($this, "permissionCheck"));
+	public $insiders = array();
+	public $id;
+	public $name;
+	public function __construct($start, $end = false, $owner = false, $name = false) {
+		if($end === false and $owner === false){
+			if(!is_string($start))
+				throw new Exception("Illegal constructor arguments for class MArea. MArea(Position \$start, Position \$end, Player \$owner) or MArea(string \$filepath) expected.");
+			if(!is_file($start))
+				throw new Exception("Illegal constructor arguments for class MArea. Argument 1 for constructing MArea from file must be file path, $start (not a file) given.");
+			$this->external = new Config($start, CONFIG_YAML);
+			$this->owners = $this->external->get("owners");
+		}
+		else{
+			$this->start = $start;
+			$this->end = $end;
+			$this->owner = $owner;
+			$this->owners[] = strtolower($owner->username);
+			if($name === false){
+				$pg = MAreaPg::request();
+				$path = $pg->a->configPath($pg)."MAreas/";
+				$id = 0;
+				while(file_exists($path."MArea-$id.yml")){
+					$id++;
+				}
+				$this->external = new Config($path."MArea-$id.yml", CONFIG_YAML, array(
+						"owners" => $this->owners,
+						"position-start" => array($start->x, $start->y, $start->z, $start->level->getName()),
+						"position-end" => array($end->x, $end->y, $end->z, $end->level->getName()),,
+						"creator" => $owner->username,
+						"flags" => 0,
+				));
+			}
+			ServerAPI::request()->addHandler("player.block.touch", array($this, "permissionCheck"));
+			ServerAPI::request()->schedule(1200, array($this, "save"), array(), true);
+		}
 	}
 	public function permissionCheck($data, $event) {
 		// flags
 		if(in_array(strtolower($data["player"]), $this->owners))
 			return;
 		$t = $data["target"];
+		if($this->checkInside($t)){
+			$data["player"]->sendChat("Umm well, I don't think this is your personal area.");
+			return false;
+		}
+	}
+	public function addOwner(Player $player){
+		$this->owners[] = strtolower($player->username); // well yes I should use $player->iusername but I am not sure
+	}
+	public function checkInside(Position $t){
 		if($t->level->getName() != $t->level->getName())
-			return;
+			return false;
 		if($t->x >= min($this->start->x, $this->end->x)
 				and $t->x <= max($this->start->x, $this->end->x)
 				// and $t->y >= min($this->start->y, $this->end->y)
 				// and $t->y <= max($this->start->y, $this->end->y)
 				and $t->z >= min($this->start->z, $this->end->z)
 				and $t->z <= max($this->start->z, $this->end->z)) {
-			// TODO configs (lang) and flags
+			return true;
 		}
-		else return;
+		return false;
 	}
-	// TODO add owner/editor functions
+	public function __destruct(){
+		$this->save();
+	}
+	public function __unset(){
+		$this->save();
+	}
+	public function save(){
+		$this->external->setAll(array(
+				"owners" => $this->owners,
+				"position-start" => array($start->x, $start->y, $start->z, $start->level->getName()),
+				"position-end" => array($end->x, $end->y, $end->z, $end->level->getName()),,
+				"creator" => $this->owner->iusername,
+				"flags" => $this->flag,
+		));
+	}
 	// FEATURE permision to enter the area/space
+	// TODO decide how to make the permissions
 }
